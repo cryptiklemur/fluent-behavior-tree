a80c45c61d9323dee8e6341c25612f91986ff893
Aaron Scherer <aequasi@gmail.com>
2017-12-05 13:35:18 -0800

    Adding default export

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import BehaviorTreeBuilder from "./BehaviorTreeBuilder";
+import BehaviorTreeStatus from "./BehaviorTreeStatus";
+import TimeData from "./TimeData";
+
+export {BehaviorTreeBuilder, BehaviorTreeStatus, TimeData}
