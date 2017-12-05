9dd71ef9ab1e61f091f94f3044e1ddff67838828
Aaron Scherer <aequasi@gmail.com>
2017-12-05 13:37:24 -0800

    Adding default export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 import BehaviorTreeBuilder from "./BehaviorTreeBuilder";
 import BehaviorTreeStatus from "./BehaviorTreeStatus";
+import BehaviorTreeNodeInterface from "./Node/BehaviorTreeNodeInterface";
+import ParentBehaviorTreeNodeInterface from "./Node/ParentBehaviorTreeNodeInterface";
+import ActionNode from "./Node/ActionNode";
+import InverterNode from "./Node/InverterNode";
+import ParallelNode from "./Node/ParallelNode";
+import SelectorNode from "./Node/SelectorNode";
+import SequenceNode from "./Node/SequenceNode";
+import BehaviorTreeErorr from "./Error/BehaviorTreeError";
+import Errors from "./Error/Errors";
 import TimeData from "./TimeData";
 
-export {BehaviorTreeBuilder, BehaviorTreeStatus, TimeData}
+export {
+    BehaviorTreeBuilder,
+    BehaviorTreeStatus,
+    TimeData,
+    BehaviorTreeNodeInterface,
+    ParentBehaviorTreeNodeInterface,
+    ActionNode,
+    InverterNode,
+    ParallelNode,
+    SelectorNode,
+    SequenceNode,
+    BehaviorTreeErorr,
+    Errors
+};
